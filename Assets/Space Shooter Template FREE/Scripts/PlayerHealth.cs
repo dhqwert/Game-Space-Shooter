369fe0ad70e8d0977bee6e5143a7c7d690df3eac
dhqwert <quandoan.c2.2005hy@gmail.com>
2026-02-12 23:13:49 +0700

    feat: Implement audio system and Game Over UI

--- Assets/Space Shooter Template FREE/Scripts/PlayerHealth.cs
+++ Assets/Space Shooter Template FREE/Scripts/PlayerHealth.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class PlayerHealth : Health
 {
+    public GameObject gameOverUI; 
+
     protected override void Die()
     {
+        gameOverUI.SetActive(true);
+
         base.Die();
+        
         Debug.Log("Player died");
     }
 }
